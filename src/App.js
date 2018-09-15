146377a7ca9d69fdb9aea3c228f697cf725400d2
Dave <andodavid95@gmail.com>
2018-09-15 18:59:36 +0200

    create new added

--- src/App.js
+++ src/App.js
@@ -4,24 +4,23 @@ import gql from 'graphql-tag';
 import { 
   ApolloProvider,
   Query,
-  Mutation,
 } from 'react-apollo';
 import { 
-  Card,
   Button,
-  Input,
+  Icon,
   Row,
   Col,
 } from 'antd';
 import Counter from './components/Counter';
 
+import Modal from './components/Modal';
 import './App.scss';
 
 const client = new ApolloClient({
   uri: "https://api-euwest.graphcms.com/v1/cjm3al9q2202u01cy0eytbw2e/master"
 })
 
-const query = gql`
+export const GET_COUNTERS = gql`
   {
     counters {
       id,
@@ -62,15 +61,32 @@ const renderCounters = counters => {
     </Col>
   )
 }
-let input;
 
+const initState = {
+  isModalVisible: false,
+  newText: "",
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {initState}
+  }
+
+  handleNewTextChange(e) {
+    e.persist();
+    console.log(e.target.value);
+    this.setState(prevState => ({
+      ...prevState,
+      newText: e.target.value,
+    }))
+  }
+
   render() {
     return (
       <ApolloProvider client={client}>
         <Query
-          query={query}
+          query={GET_COUNTERS}
         >
           {({ loading, data }) => {
             console.log(data)
@@ -88,26 +104,73 @@ class App extends Component {
           }
         }
         </Query>
-        <Mutation mutation={CREATE_COUNTER}>
+        <Row 
+          type="flex" 
+          justify="space-around"
+          align="middle"
+        >
+          <Button 
+            type="primary" 
+            onClick={() => this.setState(prevState => ({
+              ...prevState,
+              isModalVisible: true
+            }))}
+          >
+                  Add new Counter <Icon type="smile" theme="outlined" />
+          </Button>
+        </Row>
+        <Modal 
+          newText={this.state.newText}
+          isModalVisible={this.state.isModalVisible}
+          onTextChange={(e) => this.handleNewTextChange(e)}
+          onCancel={() => this.setState(initState)}
+        />
+
+{/*         
+        <Modal
+          title="Basic Modal"
+          visible={this.state.isModalVisible}
+          // onOk={this.handleOk}
+          onCancel={() => this.setState(initState)}
+          footer={[
+            <Button type="submit">Add Counter</Button>
+          ]}
+        >
+                  <Mutation mutation={CREATE_COUNTER}>
           {(createCounter, { data }) => (
             <div>
               <form
                 onSubmit={e => {
                   e.preventDefault();
-                  createCounter({ variables: { text: input.value } });
-                  input.value = "";
+                  createCounter({ variables: { text: this.state.newText } });
+                  // input.value = "";
                 }}
               >
-                <Input
-                  ref={node => {
-                    input = node;
-                  }}
-                />
-                <Button type="submit">Add Counter</Button>
+              <Row
+                type="flex" 
+                justify="space-between"
+                align="middle"
+              >
+                <Col span={11}>
+                  <Input
+                    onChange={(e) => this.handleNewTextChange(e)}
+                    value={this.state.newText}
+                  />
+                </Col>
+                <Col span={11}>
+                  <Upload>
+                    <Button>
+                      <Icon type="upload" /> Click to Upload
+                    </Button>
+                  </Upload>
+                </Col>
+              </Row>
               </form>
             </div>
           )}
         </Mutation>
+              </Modal> */}
+
       </ApolloProvider>
     );
   }
---
+++ src/components/Modal.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import PropTypes from 'react-proptypes';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+import {
+    Modal as AntdModal,
+    Button,
+    Row, 
+    Col,
+    Upload,
+    Input,
+    Icon,
+} from 'antd';
+
+import { GET_COUNTERS } from '../App.js';
+
+
+function Modal(props) {
+    const {
+        newText,
+        isModalVisible,
+        onTextChange,
+        onCancel,
+        mutate,
+    } = props;
+
+    function onSave() {
+        mutate({ 
+            variables: { 
+                text: newText
+            },
+            refetchQueries: [{ query: GET_COUNTERS }]
+        });
+        onCancel();
+    }
+
+    return (
+        <AntdModal
+          title="Basic Modal"
+          visible={isModalVisible}
+          onCancel={onCancel}
+          footer={
+            <Button onClick={() => onSave()}>Add Counter</Button>
+          }
+        >
+            <div>
+              <Row
+                type="flex" 
+                justify="space-between"
+                align="middle"
+              >
+                <Col span={11}>
+                  <Input
+                    onChange={onTextChange}
+                    value={newText}
+                  />
+                </Col>
+                <Col span={11}>
+                  <Upload>
+                    <Button>
+                      <Icon type="upload" /> Click to Upload
+                    </Button>
+                  </Upload>
+                </Col>
+              </Row>
+            </div>
+        </AntdModal>
+    )
+}
+
+const CREATE_COUNTER = gql`
+    mutation createCounter($text: String!){
+        createCounter(data: {
+            text: $text,
+            number: 0
+        }){
+        id,
+        text
+        number
+        }
+    }
+`
+
+const ModalWithMutation = graphql(
+    CREATE_COUNTER
+)(Modal);
+
+export default ModalWithMutation;
+
+Modal.propTypes = {
+    newText: PropTypes.string,
+    isModalVisible: PropTypes.bool,
+    onTextChange: PropTypes.func,
+    onCancel: PropTypes.func,
+}
